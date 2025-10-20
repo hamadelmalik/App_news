069875d6b86a4316f0c5b875079e930ce9fadc2e
hamadelmalik <hamadelmalik3@gmail.com>
2025-10-20 03:34:57 +0200

    search Feature

--- lib/modules/home/page/home_view.dart
+++ lib/modules/home/page/home_view.dart
@@ -12,103 +12,109 @@ class HomeView extends StatefulWidget {
   @override
   State<HomeView> createState() => _HomeViewState();
 }
-CategoryData?selectedCategory;
-List<CategoryData> categoryList = [
-  CategoryData(
-    categoryId: 'General',
-    categoryName: 'General',
-    categoryImage: 'assets/images/general.png',
-  ),
-  CategoryData(
-    categoryId: 'Business',
-    categoryName: 'Business',
-    categoryImage: 'assets/images/business_img.png',
-  ),
-  CategoryData(
-    categoryId: 'Sports',
-    categoryName: 'Sports',
-    categoryImage: 'assets/images/sports_img.png',
-  ),
-  CategoryData(
-    categoryId: 'Health',
-    categoryName: 'Health',
-    categoryImage: 'assets/images/health_img.png',
-  ),
-  CategoryData(
-    categoryId: 'Science',
-    categoryName: 'Science',
-    categoryImage: 'assets/images/science_img.png',
-  ),
 
-  CategoryData(
-    categoryId: 'Technology',
-    categoryName: 'Technology',
-    categoryImage: 'assets/images/technology_img.png',
-  ),
-  CategoryData(
-    categoryId: 'Entertainment',
-    categoryName: 'Entertainment',
-    categoryImage: 'assets/images/entertainment_img.png',
-  ),
+CategoryData? selectedCategory;
+List<CategoryData> categoryList = [
+  CategoryData(categoryId: 'General', categoryName: 'General', categoryImage: 'assets/images/general.png'),
+  CategoryData(categoryId: 'Business', categoryName: 'Business', categoryImage: 'assets/images/business_img.png'),
+  CategoryData(categoryId: 'Sports', categoryName: 'Sports', categoryImage: 'assets/images/sports_img.png'),
+  CategoryData(categoryId: 'Health', categoryName: 'Health', categoryImage: 'assets/images/health_img.png'),
+  CategoryData(categoryId: 'Science', categoryName: 'Science', categoryImage: 'assets/images/science_img.png'),
+  CategoryData(categoryId: 'Technology', categoryName: 'Technology', categoryImage: 'assets/images/technology_img.png'),
+  CategoryData(categoryId: 'Entertainment', categoryName: 'Entertainment', categoryImage: 'assets/images/entertainment_img.png'),
 ];
 
 class _HomeViewState extends State<HomeView> {
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text(selectedCategory== selectedCategory ? "Home" : selectedCategory!.categoryName)),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Icon(Icons.search_sharp, size: 38),
-          ),
-        ],
-      ),
-      drawer: CustomDrawerView(
-        onTap: () {
-          setState(() {
-            selectedCategory = null;
-            navigatorKey.currentState!.pop();
-          });
-        },
-      ),
-      body: selectedCategory== null ? Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                Text(
-                  "Good Morning\n Here is Some News For You",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
-                ),
-                Gap(20),
-                ...categoryList.map((categoryData) {
-                  return CategoryItem(
-                    categoryData: categoryData,
-                    index: categoryList.indexOf(categoryData),
-                    onTap:onSelectCategory ,
-                  );
-                }),
-              ],
-            ),
-          ),
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+
+        drawer: CustomDrawerView(
+          onTap: () {
+            setState(() {
+              selectedCategory = null;
+              navigatorKey.currentState!.pop();
+            });
+          },
         ),
-      ): ArticlesView(selectedCategory: selectedCategory!,),
+        body: Column(
+          children: [
+      
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                border: Border.all(color: Colors.black),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                child: TextField(
+                controller: searchController,
+                style: TextStyle(color:Colors.black,),
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  hintText: 'Search...',
+                  hintStyle: TextStyle(color: Colors.black),
+                  prefixIcon: Icon(Icons.search, color: Colors.black),
+                  suffixIcon: Icon(Icons.cancel, color: Colors.red),
+                  filled: true,
+      
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(color: Colors.white38, width: 2),
+      
+      
+                  ),
+                ),
+                onChanged: (value) {
+                  setState(() {}); // يعيد بناء ArticlesView بالكلمة الجديدة
+                },
+              ),
+            ),
+      
+      
+            Expanded(
+              child: selectedCategory == null
+                  ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Text(
+                        "Good Morning\nHere is Some News For You",
+                        style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                      ),
+                      Gap(20),
+                      ...categoryList.map((categoryData) {
+                        return CategoryItem(
+                          categoryData: categoryData,
+                          index: categoryList.indexOf(categoryData),
+                          onTap: onSelectCategory,
+                        );
+                      }),
+                    ],
+                  ),
+                ),
+              )
+                  : ArticlesView(
+                selectedCategory: selectedCategory!,
+                searchKeyword: searchController.text,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+
+
   }
+
   void onSelectCategory(CategoryData categoryData) {
     setState(() {
       selectedCategory = categoryData;
     });
   }
 }
-
-// void onSelectCategory(CategoryData categoryData) {
-//   setState(() {
-//     selectedCategory = categoryData;
-//   });
-// }
-// }
